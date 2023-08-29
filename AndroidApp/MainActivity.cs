9fc72dc235d2a40509a0eae3348ef8d2172a93cc
piccoliprado <113219544+piccoliprado@users.noreply.github.com>
2023-08-29 19:57:26 -0300

    Update MainActivity.cs

--- AndroidApp/MainActivity.cs
+++ AndroidApp/MainActivity.cs
@@ -6,7 +6,7 @@ namespace AndroidApp
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-
+            //HM. COMENTEI
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
         }
