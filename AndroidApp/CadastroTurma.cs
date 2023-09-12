660218821b8c9c2b20395ccae897d67093c8afce
jailtoncoelho <jailtonjscoelho@gmail.com>
2023-09-12 19:10:26 -0300

    added menu to main activity

---
+++ AndroidApp/CadastroTurma.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidApp
+{
+    internal class CadastroTurma : Activity
+    {
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //HM. COMENTEI
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_cadastro_turma);
+            // comentario para branchhh
+        }
+    }
+}
