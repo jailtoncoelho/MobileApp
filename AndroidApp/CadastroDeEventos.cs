829d1437dc1d51fe5d14e81a43798a5135a6efdf
a <a@a.com>
2023-09-12 21:11:54 -0300

    view criada
    criado view cadastro de eventos

---
+++ AndroidApp/CadastroDeEventos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AndroidApp
+{
+    public class CadastroDeEventos
+    {
+        public int edtNomeEvento { get; set; }
+        public int edtDescricaoEvento { get; set; }
+        public int edtResponsavelEvento { get; set; }
+        public int rdGrupos { get; set; }
+    }
+}
