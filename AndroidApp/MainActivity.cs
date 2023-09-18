a5970dae1ba6a69cf1e64ed3a38de39122e94f14
jailtoncoelho <jailtonjscoelho@gmail.com>
2023-09-18 16:49:19 -0300

    adds a menu to the main screen and its icons

--- AndroidApp/MainActivity.cs
+++ AndroidApp/MainActivity.cs
@@ -1,6 +1,8 @@
 using Android.Content;
 using Android.Views;
 
+
+
 namespace AndroidApp
 {
     [Activity(Label = "@string/app_name", MainLauncher = true)]
@@ -12,39 +14,7 @@ namespace AndroidApp
             //HM. COMENTEI
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-            // comentario para branchhh
         }
 
-
-
-
-        public override bool OnCreateOptionsMenu(IMenu? menu)
-        {
-            MenuInflater.Inflate(Resource.Layout.activity_main, menu);
-            return base.OnCreateOptionsMenu(menu);
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            // Handle item selection
-            switch (item.ItemId)
-            {
-                case Resource.Id.cadastro_turma:
-                    ShowsCadastroTurma();
-                    return true;
-                case Resource.Id.sobre:
-                    //showsSobre();
-                    return true;
-                default:
-                    return base.OnOptionsItemSelected(item);
-
-            }
-        }
-
-        private void ShowsCadastroTurma()
-        {
-            Intent myIntent = new(this, typeof(CadastroTurma));
-            StartActivity(myIntent);
-        }
     }
 }
