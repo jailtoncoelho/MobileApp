4609f24e8037a862b9e4dad604bfd039c5db0b8b
MarcioR <113216304+MarcioRdgs@users.noreply.github.com>
2023-09-26 23:12:53 -0300

    Realizado summary no arquivo LoginActivity.cs

--- AndroidApp/Activities/LoginActivity.cs
+++ AndroidApp/Activities/LoginActivity.cs
@@ -4,7 +4,11 @@ namespace AndroidApp.Activities
 {
     [Activity(Name = "com.ifpr_telemacoborba.alerts.LoginActivity")]
     internal class LoginActivity : Activity
-    {
+    {/// <summary>
+    /// Metódo onCreate é chamado quando a atividade é iniciada.
+    /// </summary>
+    /// <param name="savedInstanceState"></param>
+        
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
