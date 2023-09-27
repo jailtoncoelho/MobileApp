b15c47a94367cbb9c79731fd7f7ae92edf12fd62
Matheus sá <matpedrososa@gmail.com>
2023-09-26 21:09:38 -0300

    Criado metodo

--- AndroidApp/Activities/CadastroAlunosActivity.cs
+++ AndroidApp/Activities/CadastroAlunosActivity.cs
@@ -14,12 +14,18 @@ namespace AndroidApp.Activities
         /// <param name="savedInstanceState"></param>
         protected override void OnCreate(Bundle? savedInstanceState)
         {
-           
+
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
+           
+            ///Metodo que puxa as informaçoes na tela 
+            
+            MostrarPage();
+        }
+
+        private void MostrarPage()
+        {
             SetContentView(Resource.Layout.activity_cadastro_alunos);
-
-
         }
     }
 }
