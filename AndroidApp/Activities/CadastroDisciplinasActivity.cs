14432ca8df702fd92842abbeb012d3be0eb4f07c
a <a>
2023-10-24 22:05:48 -0300

    Correção cadastro usuario

--- AndroidApp/Activities/CadastroDisciplinasActivity.cs
+++ AndroidApp/Activities/CadastroDisciplinasActivity.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Firebase.Database;
+using Newtonsoft.Json;
 
 namespace AndroidApp.Activities
 {
@@ -12,14 +14,55 @@ namespace AndroidApp.Activities
     internal class CadastroDisciplinasActivity : Activity
     {
         /// <summary>
-        /// Metodo que chama a tela atraves do Id da Avtivity
+        /// Metodo que chama a tela atraves do Id da Activity
         /// </summary>
         /// <param name="savedInstanceState"></param>
         protected override void OnCreate(Bundle? savedInstanceState)
         {
+
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
-            SetContentView(Resource.Layout.activity_cadastro_disciplinas);
+
+            ///Metodo puxa pagina na main 
+
+            MostrarPage();
+            Button? btnEnviar = FindViewById<Button>(Resource.Id.btnConfirm);
+            if (btnEnviar != null)
+            {
+                // Associe um evento de clique
+                btnEnviar.Click += (sender, e) =>
+                {
+                    // O botão foi clicado, execute o código desejado aqui
+                    // Por exemplo, exiba uma mensagem
+                    CadastraDisciplinaAsync();
+                };
+            }
+        }
+        private async Task CadastraDisciplinaAsync()
+        {
+
+            // Crie um objeto com os dados que deseja salvar
+            var dados = new
+            {
+                Nome = "@+id/textView1",
+                Disciplina = "@+id/textView2"
+            };
+
+            // Converta o objeto para JSON
+            string jsonDados = JsonConvert.SerializeObject(dados);
+
+
+
+            FirebaseClient firebase = new FirebaseClient("https://ifpr-alerts-default-rtdb.firebaseio.com/");
+            await firebase
+                .Child("disciplinas")
+                .PostAsync(jsonDados);
+
+
+        }
+        private void MostrarPage()
+        {
+            SetContentView(Resource.Layout.activity_cadastro_usuario);
         }
     }
 }
---
+++ AndroidApp/Activities/CadastroUsuarioActivity.cs
@@ -0,0 +1,101 @@
+﻿using Android.Content;
+using Firebase.Database;
+using Firebase.Database.Core.View;
+using Newtonsoft.Json;
+using Firebase;
+using Google.Android.Material.BottomNavigation;
+
+
+namespace AndroidApp.Activities
+{
+    /// <summary>
+    /// Criado Activity para a tela Cadastro de alunos
+    /// </summary>
+    [Activity(Name = "com.ifpr_telemacoborba.alerts.CadastroUsuarioActivity")]
+    internal class CadastroUsuarioActivity : Activity
+    {
+        /// <summary>
+        /// Metodo OnCreate é chamado quando a tela é criada e intancia o estado
+        /// </summary>
+        /// <param name="savedInstanceState"></param>
+        protected override void OnCreate(Bundle? savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+
+            ///Metodo puxa pagina na main 
+
+            MostrarPage();
+            Button? btnEnviar = FindViewById<Button>(Resource.Id.btnConfirm);
+            if (btnEnviar != null)
+            {
+                // Associe um evento de clique
+                btnEnviar.Click += (sender, e) =>
+                {
+                    // O botão foi clicado, execute o código desejado aqui
+                    // Por exemplo, exiba uma mensagem
+                    CadastraUserAsync();
+                };
+            }
+        }
+
+
+        private async Task CadastraUserAsync()
+        {
+
+            var nomeUser = FindViewById<EditText>(Resource.Id.editTextName);
+            var senhaUser = FindViewById<EditText>(Resource.Id.editTextPassword);
+            var confSenhaUser = FindViewById<EditText>(Resource.Id.editTextConfirmPassword);
+            var emailUser = FindViewById<EditText>(Resource.Id.editTextEmail);
+
+            if (senhaUser == confSenhaUser)
+            {
+
+
+
+                // Crie um objeto com os dados que deseja salvar
+                var dados = new
+                {
+                    Nome = nomeUser?.Text,
+                    Senha = senhaUser?.Text,
+                    Email = emailUser?.Text,
+                };
+
+                // Converta o objeto para JSON
+                string jsonDados = JsonConvert.SerializeObject(dados);
+
+
+                FirebaseClient firebase = new FirebaseClient("https://ifpr-alerts-default-rtdb.firebaseio.com/");
+                var result = await firebase
+                    .Child("usuarios")
+                    .PostAsync(jsonDados);
+
+                if (result != null)
+                {
+                    // reinicia valores dos campos da tela
+                    nomeUser.Text = "";
+                    senhaUser.Text = "";
+                    emailUser.Text = "";
+
+                    Toast.MakeText(this, "Evento cadastrado com sucesso!", ToastLength.Short)?.Show();
+                }
+                else
+                {
+                    Toast.MakeText(this, "O evento não pôde ser cadastrado!", ToastLength.Short)?.Show();
+                }
+            }
+            else
+            {
+                Toast.MakeText(this, "A senha está invalida!", ToastLength.Short)?.Show();
+            }
+        }
+
+        private void MostrarPage()
+        {
+            SetContentView(Resource.Layout.activity_cadastro_usuario);
+        }
+    }
+
+
+}
