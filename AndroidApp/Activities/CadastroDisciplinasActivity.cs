97921b53a93ca0354e1d4a1dd58ba4e0ca2486bc
jonathan2734 <jonathan2734@gmail.com>
2023-10-24 22:11:21 -0300

    Adicionado cadastro de Disciplinas

--- AndroidApp/Activities/CadastroDisciplinasActivity.cs
+++ AndroidApp/Activities/CadastroDisciplinasActivity.cs
@@ -5,6 +5,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Firebase;
+using Firebase.Database;
+using Xamarin.Essentials;
+using FirebaseOptions = Firebase.FirebaseOptions;
+using Newtonsoft.Json;
 
 namespace AndroidApp.Activities
 {
@@ -20,6 +25,62 @@ namespace AndroidApp.Activities
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_cadastro_disciplinas);
+
+            Button? btnEnviar = FindViewById<Button>(Resource.Id.btnEnviar);
+            if (btnEnviar != null)
+            {
+                // Associe um evento de clique
+                btnEnviar.Click += (sender, e) =>
+                {
+                    // O botão foi clicado, execute o código desejado aqui
+                    // Por exemplo, exiba uma mensagem
+                    CriaNoDisciplinasSeNaoExistirAsync();
+                };
+            }
+        }
+        private void Enviar_Click(object sender, System.EventArgs e)
+        {
+            // O botão foi clicado, execute o código desejado aqui
+            // Por exemplo, exiba uma mensagem
+            CriaNoDisciplinasSeNaoExistirAsync();
+        }
+
+        private async Task CriaNoDisciplinasSeNaoExistirAsync()
+        {
+            var nomeAluno = FindViewById<EditText>(Resource.Id.edtNomeAluno);
+            var nomeDisciplina = FindViewById<EditText>(Resource.Id.edtNomeDisciplina);
+            var dataDisciplina = FindViewById<DatePicker>(Resource.Id.datePickerDataDisciplina);
+
+            // Crie um objeto com os dados que deseja salvar
+            var dados = new
+            {
+                Nome = nomeAluno?.Text,
+                Disciplina = nomeDisciplina?.Text,
+                Data = dataDisciplina?.DateTime.ToShortDateString()
+            };
+
+            // Converta o objeto para JSON
+            string jsonDados = JsonConvert.SerializeObject(dados);
+
+            FirebaseClient firebase = new FirebaseClient("https://ifpr-alerts-default-rtdb.firebaseio.com/");
+            var result = await firebase
+                .Child("disciplinas")
+                .PostAsync(jsonDados);
+
+            if (result != null)
+            {
+                // reinicia valores dos campos da tela
+                nomeAluno.Text = "";
+                nomeDisciplina.Text = "";
+                dataDisciplina.DateTime = DateTime.Now;
+
+                Toast.MakeText(this, "Disciplina cadastrada com sucesso!", ToastLength.Short)?.Show();
+            }
+            else
+            {
+                Toast.MakeText(this, "A disciplina não pôde ser cadastrada!", ToastLength.Short)?.Show();
+            }
+
         }
     }
 }
